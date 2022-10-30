4c430c82adf7ccbe7b1c9b35910d810bb0385dac
liamtran <quyettk@zen-s.com>
2022-10-30 20:36:55 +0700

    day2

---
+++ code-example/day1/5-modules.js
@@ -0,0 +1,8 @@
+const names = require("./1-name");
+const sayHiFunc = require("./2-ultils");
+require("./4-mind-grenade");
+
+console.log(names);
+console.log(sayHiFunc);
+
+sayHiFunc(names.name2);
--- code-example/day1/index.js
+++ code-example/day1/index.js
@@ -6,11 +6,17 @@
 // module - info about current module (file)
 // process - info about env where the program is being excuted
 
-const names = require("./1-name");
-const sayHiFunc = require("./2-ultils");
-console.log(names);
-console.log(sayHiFunc);
+const path = require("path");
 
-sayHiFunc(names.name2);
+console.log(path.sep);
 
-// console.log(__dirname);
+const filePath = path.join("/content/", "subfolder", "test.txt");
+
+console.log(filePath);
+
+const base = path.basename(filePath);
+console.log(base);
+
+const absolute = path.resolve(__dirname, "content", "subfolder", "test.txt");
+
+console.log(absolute);
